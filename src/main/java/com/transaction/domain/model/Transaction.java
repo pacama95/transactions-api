97123797983937458aba0086a93d0275457f0132
Pablo Cazorla <pablocazorla95@gmail.com>
2025-09-18 23:19:22 +0200

    Fix tests, pending to fix issue with transaction ID in domain event

--- src/main/java/com/transaction/domain/model/Transaction.java
+++ src/main/java/com/transaction/domain/model/Transaction.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Getter
-@AllArgsConstructor
+@AllArgsConstructor(onConstructor = @__({@Default}))
 public class Transaction {
     private final UUID id;
     private String ticker;
@@ -44,35 +44,6 @@ public class Transaction {
                        BigDecimal fractionalMultiplier,
                        Currency commissionCurrency) {
         this.id = null;
-        this.ticker = ticker;
-        this.transactionType = transactionType;
-        this.quantity = quantity;
-        this.price = price;
-        this.fees = fees;
-        this.currency = currency;
-        this.transactionDate = transactionDate;
-        this.notes = notes;
-        this.isActive = isActive;
-        this.isFractional = isFractional;
-        this.fractionalMultiplier = fractionalMultiplier;
-        this.commissionCurrency = commissionCurrency;
-    }
-
-    @Default
-    public Transaction(UUID id,
-                       String ticker,
-                       TransactionType transactionType,
-                       BigDecimal quantity,
-                       BigDecimal price,
-                       BigDecimal fees,
-                       Currency currency,
-                       LocalDate transactionDate,
-                       String notes,
-                       Boolean isActive,
-                       Boolean isFractional,
-                       BigDecimal fractionalMultiplier,
-                       Currency commissionCurrency) {
-        this.id = id;
         this.ticker = ticker;
         this.transactionType = transactionType;
         this.quantity = quantity;
